2de02761e1e9526c11f73c523c980bc240ee3c02
Cristian Arboleda <carboleda@merqueo.com>
2023-07-13 21:05:55 -0500

    initial commit

---
+++ app/src/main/java/com/cristian/sistecreditotestapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.cristian.sistecreditotestapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.cristian.sistecreditotestapp.ui.theme.SisteCreditoTestAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SisteCreditoTestAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                }
+            }
+        }
+    }
+}
